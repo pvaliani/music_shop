eee0da1312da8a38b4db5776d1d4a8df596038b4
Pedram Valiani <p.valiani@gmail.com>
2021-02-07 15:08:21 +0000

    set up instrument test

--- src/test/java/MusicalInstrumentTest.java
+++ src/test/java/MusicalInstrumentTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MusicalInstrumentTest {
+
+    @Before
+    public void setUp(){
+
+    }
+
+
+    @Test
+    public void firstTest(){
+        assertEquals(1,1);
+    }
+
 }
