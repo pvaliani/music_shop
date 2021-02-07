f6c94abc5ca76593515c7cc18b395447cd793e50
Pedram Valiani <p.valiani@gmail.com>
2021-02-07 15:13:26 +0000

    gives music shop a name and a till

--- src/main/java/MusicShop.java
+++ src/main/java/MusicShop.java
@@ -1,2 +1,11 @@
-public class MusicShop {
+public abstract class MusicShop {
+
+    private String name;
+    private double till;
+
+    public MusicShop(String name, double till){
+        this.name = name;
+        this.till =  till;
+    }
+
 }
