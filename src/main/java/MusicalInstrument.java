77204419613c79df8286fb1164145b1af84d6e60
Pedram Valiani <p.valiani@gmail.com>
2021-02-07 15:06:30 +0000

    adds getter and setters for instruments, declares instrument properties

--- src/main/java/MusicalInstrument.java
+++ src/main/java/MusicalInstrument.java
@@ -1,2 +1,32 @@
 public class MusicalInstrument {
+
+    private String name;
+    private String brand;
+
+//    An instrument will have common properties of a name and brand which can be passed down
+//    to the sub-class
+
+    public MusicalInstrument(String name, String brand){
+        this.name = name;
+        this.brand = brand;
+    }
+
+//    getters and setters for the instrument name and brand for the shop to be able
+//    to input information about the items in the store
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 }
