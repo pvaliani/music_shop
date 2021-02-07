85556cb46fe3a9cc43c81bea4e852ad7ff088bc3
Pedram Valiani <p.valiani@gmail.com>
2021-02-07 14:58:54 +0000

    file structure set up and iPlay interface set up - may change later

---
+++ src/main/java/IPlay.java
@@ -0,0 +1,5 @@
+// Interface contract promises the String method of playInstrument
+
+public interface IPlay {
+    public String playInstrument();
+}
---
+++ src/main/java/MusicalInstrument.java
@@ -0,0 +1,2 @@
+public class MusicalInstrument {
+}
