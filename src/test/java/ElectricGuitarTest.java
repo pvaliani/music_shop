aad1b0df5417f06c0dcf0a2335e000f66dd28127
Pedram Valiani <p.valiani@gmail.com>
2021-02-07 17:43:14 +0000

    Extensions mostly complete but issues with tests

--- src/test/java/ElectricGuitarTest.java
+++ src/test/java/ElectricGuitarTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ElectricGuitarTest {
+
+    private ElectricGuitar electricGuitar;
+
+    @Before
+    public void setUp(){
+        electricGuitar = new ElectricGuitar("Ibanez", "RGA", 500, 1000, true, 7);
+
+    }
+
+    @Test
+    public void canGetGuitarName(){
+        assertEquals("Ibanez", electricGuitar.getName());
+    }
+
+    @Test
+    public void canGetGuitarModel(){
+
+    }
+
 }
